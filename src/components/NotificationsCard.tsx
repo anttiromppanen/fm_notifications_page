b5b399b80f7817e28327600eeb20216fae7bef46
Antti Romppanen <romppanen.antti@gmail.com>
2023-09-29 00:04:18 +0300

    add shadow

--- src/components/NotificationsCard.tsx
+++ src/components/NotificationsCard.tsx
@@ -10,8 +10,8 @@ function NotificationsCard() {
   return (
     <div
       className="
-      h-full w-full max-w-[730px] bg-userCardBgColor px-4 py-6 md:mt-14
-      md:h-fit md:rounded-xl md:px-8 md:py-9"
+      h-full w-full max-w-[730px] bg-userCardBgColor px-4 py-6 shadow-2xl shadow-slate-200
+      md:mt-14 md:h-fit md:rounded-xl md:px-8 md:py-9"
     >
       <NotificationsTopBar />
       <div className="mt-6 flex flex-col gap-y-4">
